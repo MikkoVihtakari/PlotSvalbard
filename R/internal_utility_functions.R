b2a2dd6117e840d53b4407f4f4cc5af4555ad56e
MikkoVihtakari <mikko.vihtakari@gmail.com>
2018-12-18 21:48:37 +0100

    Improved section_plot. Other minor bugfixes.

--- R/internal_utility_functions.R
+++ R/internal_utility_functions.R
@@ -38,12 +38,35 @@ round_any <- function(x, accuracy, f = round) {
   f(x / accuracy) * accuracy
 }
 
+#' @title Round to pretty log breaks
+#' @param x numeric vector to round
+#' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or
+#'  \code{\link{round}}
+#' @keywords internal
+#' @author Mikko Vihtakari
+#' @seealso \code{\link{round_any}}
+#' @export
+#'
+pretty_log <- function(x, f = round) {
+  x <- round(x, 0)
+
+  ifelse(nchar(x) == 1, round_any(x, 5),
+    ifelse(nchar(x) == 2 & x < 20, round_any(x, 5),
+      ifelse(nchar(x) == 2 & x < 90, round_any(x, 10),
+        ifelse(nchar(x) == 2, round_any(x, 100),
+          ifelse(nchar(x) == 3 & x < 500, round_any(x, 50),
+            ifelse(nchar(x) == 3, round_any(x, 100),
+              ifelse(nchar(x) == 4, round_any(x, 1000),
+                ifelse(nchar(x) == 5, round_any(x, 10000), round_any(x, 1e5)))))))))
+}
+
+
 #' @title Convert direct UTM distances to kilometers
 #' @description Divides \code{x} by 1000 (i.e from meters to kilometers, since polar stereographic are meters from the North Pole). Used for ggplot2 labels.
 #' @param x numeric to be converted
 #' @keywords internal
 #' @export
-#' 
+#'
 formatterUTMkm <- function(x){
     x/1000
 }
